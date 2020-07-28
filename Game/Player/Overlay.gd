bf2f3b97e5ea812ce8458f1b80601c75a8ce31cd
Ktosox <ktoso3@gmail.com>
2020-07-28 20:16:25 +0200

    added fonts for top game bar

--- Game/Player/Overlay.gd
+++ Game/Player/Overlay.gd
@@ -4,16 +4,16 @@ extends CanvasLayer
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-onready var Item1 = $Screen/Top/Item1/Ammount
-onready var Item2 = $Screen/Top/Item2/Ammount
-onready var Level = $Screen/Top/Level/Ammount
-onready var Followers = $Screen/Top/Followers/Ammount
+onready var Item1 = $Screen/Top/Background/HBoxContainer/Panel1/Item1/Ammount
+onready var Item2 = $Screen/Top/Background/HBoxContainer/Panel2/Item2/Ammount
+onready var Level = $Screen/Top/Background/HBoxContainer/Panel4/Level/Ammount
+onready var Followers = $Screen/Top/Background/HBoxContainer/Panel3/Followers/Ammount
 var optionsWindowScene = preload("res://OptionsMenu.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if(GM.current_level != null):
-		$Screen/Top/Level/Ammount.text = String(GM.current_level.ID)
+		Level.text = String(GM.current_level.ID)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
